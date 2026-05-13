40a171d5da64bbb00a2cb580a923a75e5c8cb7f7
samueejk-collab <samuelhuallpa@etec.uba.ar>
2026-05-13 12:01:37 -0300

    guardo ejercicios  map

--- colecciones-funcionales-y-lambdas/src/main/kotlin/ProductoManager.kt
+++ colecciones-funcionales-y-lambdas/src/main/kotlin/ProductoManager.kt
@@ -19,18 +19,19 @@ class ProductoManager {
     // Parte A: Operaciones con Map
 
     fun obtenerNombres(productos: List<Producto>): List<String> {
-        TODO("Implementar: Debe retornar una lista con solo los nombres de los productos")
+        return productos.map { it.nombre }
+
     }
 
     fun aplicarDescuento(
         productos: List<Producto>,
         descuentoPorcentaje: Double,
     ): List<Double> {
-        TODO("Implementar: Debe retornar una lista con los precios después de aplicar el descuento")
+        return productos.map { it.precio - (it.precio * (descuentoPorcentaje / 100.0)) }
     }
 
     fun generarEtiquetas(productos: List<Producto>): List<String> {
-        TODO("Implementar: Debe generar etiquetas en formato 'nombre - \$precio (estado)'")
+        return productos.map {"${it.nombre} - $${it.precio} (${if (it.enStock) "Disponible" else "Agotado"})"}
     }
 
     // Parte B: Operaciones con Filter
